f4537c9074ae6e02518f091b58ddad20d0260b4e
Yuri1660 <iyra42rus@gmail.com>
2024-07-05 14:59:08 +0700

    Add files via upload

---
+++ homework4.py
@@ -0,0 +1,9 @@
+my_string = input("Введите вашу дату рождения: ")
+lenght_of_string = len(my_string)
+print(len(my_string))
+print('my_string'.upper())
+print('my_string'.lower())
+print('my string'.replace(' ',''))
+example = 'my_string'
+print(example[0])
+print(example[-1])
